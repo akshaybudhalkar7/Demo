e00f9f33e4d17e675f9c48923db968bc4bd4d71c
TruptiAB <165988003+TruptiAB@users.noreply.github.com>
2024-08-13 22:37:31 +0530

    added infra

---
+++ Infrastucture/app.py
@@ -0,0 +1,17 @@
+from aws_cdk import App, Environment
+
+from cdk.cdk_stack import DemoStack
+
+app = App()
+
+DemoStack(
+    app,
+    "demo-integration",
+    environment = "sbx",
+    env = Environment(
+        account = '441058194263',
+        region = 'us-east-1'
+    )
+)
+
+app.synth()
---
+++ Infrastucture/cdk/cdk_stack.py
@@ -0,0 +1,15 @@
+from os import path
+from aws_cdk import aws_s3, Stack
+from constructs import Construct
+
+
+
+class DemoStack(Stack):
+    def __init__(self, scope: Construct, id:str, environment:None, **kwargs) -> None:
+        super().__init__(scope,id,**kwargs)
+
+        sample_bucket = aws_s3.Bucket(
+            self,
+            "%s-s3" % id,
+            bucket_name="%s-s3" % id,
+        )
